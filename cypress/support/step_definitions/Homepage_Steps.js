c656cf0107dca9f70a062d0a57e8bbd2ccd9a0d2
namrata <tonamrataagrawal@gmail.com>
2023-07-13 21:38:17 +0530

    first commit

---
+++ cypress/support/step_definitions/Homepage_Steps.js
@@ -0,0 +1,9 @@
+
+import { Given } from "@badeball/cypress-cucumber-preprocessor";
+const url="http://www.webdriveruniversity.com/";
+
+Given (`I navigate to the webdriveruniversity homepage`,() => {
+
+    cy.visit(url);
+})
+   
